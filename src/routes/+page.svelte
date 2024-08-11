e7934a37e9cddf2c9b9583ffc5377480b28e6450
Eric - Wall Sound <emoolenbeek@gmail.com>
2024-08-11 08:56:52 -0400

    change input to type search

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,7 @@
 
 <div class="container h-full mx-auto flex justify-center items-center">
 	<div class="space-y-5 py-5">
-		<input class="input" title="Input (text)" type="text" bind:value={text} placeholder="type here" />
+		<input class="input" title="Input (text)" type="search" bind:value={text} placeholder="type here" />
 		<h1>{text || "some text"}</h1>
 	</div>
 </div>
